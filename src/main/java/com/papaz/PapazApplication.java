32251ee9ce47b9e523cb84ad2d6dfbf2bc268cfe
AliGunayy <86783622+AliGunayy@users.noreply.github.com>
2021-07-01 16:15:33 +0300

    Add files via upload

---
+++ src/main/java/com/papaz/PapazApplication.java
@@ -0,0 +1,13 @@
+package com.papaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapazApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/papaz/PapazApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
